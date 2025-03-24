b11180e271a90f3128f62b07776230d1cc33ee4e
h1l3xx <alksmlkv@gmail.com>
2025-03-24 16:40:47 +0300

    feat(login): add notification for login error

--- app/src/main/java/com/rmp/ui/login/LoginScreen.kt
+++ app/src/main/java/com/rmp/ui/login/LoginScreen.kt
@@ -1,15 +1,18 @@
 package com.rmp.ui.login
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+
 import com.rmp.R
 import com.rmp.ui.components.AccentButton
 import com.rmp.ui.components.AppScreen
@@ -23,6 +26,8 @@ fun LoginScreen(
     onLoginClick: () -> Unit,
     onBackClick: () -> Unit
 ) {
+
+    val context = LocalContext.current
 
     AppScreen {
         Column(
@@ -80,10 +85,24 @@ fun LoginScreen(
 
             Spacer(modifier = Modifier.weight(1f))
 
+            val notification = if (uiState.loginError != null) {
+                    stringResource(R.string.login_error)
+            } else {
+                null
+            }
+
             AccentButton(
                 stringResource(R.string.login),
-                onLoginClick,
-            )
+            ) {
+                if (notification != null) {
+                    Toast.makeText(
+                        context,
+                        notification,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                onLoginClick()
+            }
 
             SecondaryButton(
                 stringResource(R.string.prev),
